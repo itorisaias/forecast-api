55702e1074a5352308055347c90e257776f3a21b
itor isaias da silva <itor.isaias@gmail.com>
2020-08-07 18:56:07 -0300

    chore(setup): init project and add typescript, scripts, module alias

---
+++ src/utils/module-alias.ts
@@ -0,0 +1,9 @@
+import * as paths from 'path';
+import moduleAlias from 'module-alias';
+
+const files = paths.resolve(__dirname, '../..');
+
+moduleAlias.addAliases({
+  '@src': paths.join(files, 'src'),
+  '@test': paths.join(files, 'test')
+});
